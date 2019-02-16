eb349fe8089eb8c2da534f7f2419b195e38eea1f
Sergey <s.maskin@mail.ru>
2019-02-16 15:53:09 +0300

    lesson3

---
+++ es/3/main.js
@@ -0,0 +1,88 @@
+// 1. Переделайте makeGETRequest() так, чтобы она использовала промисы.
+// 2. Добавьте в соответствующие классы методы добавления товара в корзину, удаления товара из корзины и получения списка товаров корзины.
+// 3* Переделайте GoodsList так, чтобы fetchGoods() возвращал промис, а render() вызывался в обработчике этого промиса.
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+class CartItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `<a class="dropdown-item" href="#">${this.title}&nbsp;<span class="badge badge-primary">${this.price}$</span>&nbsp;<button class="remove btn btn-danger btn-sm">x</button></a>`;
+  }
+}
+
+class Cart {
+  constructor() {
+    this.items = [];
+  }
+  init() {
+    let that = this;
+    Array.from(document.querySelectorAll('.add')).forEach(item => {
+      item.addEventListener('click', function (event) {
+        that.items.push({
+          title: event.target.dataset.title,
+          price: event.target.dataset.price
+        });
+        that.render();
+      });
+    });
+  }
+  getTotal() {
+    return this.items.reduce((total, item) => total + item.price);
+  }
+  render() {
+    document.querySelector('.cart').innerHTML = this.items.map(item => (new CartItem(item.title, item.price)).render()).join('');
+  }
+}
+
+class Product {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `
+<div class="col-lg-4 col-md-6 mb-4">
+    <div class="card h-100">
+        <a class="img-wrap" href="#"><img class="card-img-top" src="https://placehold.it/200x150" alt="${this.title}"></a>
+        <div class="card-body">
+            <h5 class="card-title">
+                <a class="text-info" href="#">${this.title}</a>
+            </h5>
+            <h5>${this.price}</h5>
+        </div>
+        <div class="card-footer"><button class="add btn btn-primary" data-title="${this.title}" data-price="${this.price}"><i class="fa fa-plus"></i>&nbsp;Добавить</button></div>
+    </div>
+</div>
+`;
+  }
+}
+
+class ProductList {
+  constructor() {
+    this.items = [];
+  }
+  fetchItems() {
+    fetch(`${API}/catalogData.json`)
+      .then(result => result.json())
+      .then(data => {
+        this.items = data;
+        this.render();
+      })
+      .catch(error => console.log(error));
+  }
+  render() {
+    document.querySelector('.products').innerHTML = this.items.map(item => (new Product(item.product_name, item.price)).render()).join('');
+  }
+}
+
+const productList = new ProductList();
+productList.fetchItems();
+productList.render();
+
+const cart = new Cart();
+window.onload = () => cart.init();
+
